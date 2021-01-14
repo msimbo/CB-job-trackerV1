88109221a621851a7be95b0b8697abf8f8846856
Xparham <xparham@gmail.com>
2021-01-14 17:03:49 -0500

    created selectors for form, keyword and location

---
+++ index.js
@@ -0,0 +1,7 @@
+const searchForm = document.querySelector("#search-form")
+
+const keywordSearch = document.querySelector("#keyword-search")
+
+const locationSearch = document.querySelector("#location-search")
+
+console.log(keywordSearch)
